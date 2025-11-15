980b817f9e9f3272bcf583ca8c3f30cb287709fd
Oskars Sylwan <30318587+oskarssylwan@users.noreply.github.com>
2025-11-15 11:50:45 +0100

    fix(app): forward headers (#40)
    * chore(sdk-react): remove workspaceId
    
    * fix(app): forward headers

--- app/cli/proxy-internal.js
+++ app/cli/proxy-internal.js
@@ -9,7 +9,7 @@ export function ProxyInternal() {
 
         const domain = process.env.OSSY_API_URL || 'https://api.ossy.se'
         const url = `${domain}${req.originalUrl?.replace('/@ossy', '/api/v0')}`
-        const headers = JSON.parse(JSON.stringify(req.headers)) // Clone headers
+        const headers = { ...(req.headers || {}) } // Clone headers
         const workspaceId = headers.workspaceId
     
         if (workspaceId) {
