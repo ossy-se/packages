ffbd95c3e1e1d285f650554ea115c8f02025bdde
Oskars Sylwan <30318587+oskarssylwan@users.noreply.github.com>
2026-01-11 00:37:43 +0100

    fix(connected-components): build (#58)
    * fix(app): isAuthenticated
    
    * fix(app, connected-components): send initial app config to the client
    
    * fix(connected-components): export
    
    * feat(connected-components): see pages in theme editor
    
    * fix(connected-components): build

--- connected-components/src/app/AppContext.js
+++ connected-components/src/app/AppContext.js
@@ -1,4 +1,5 @@
 import React, { createContext } from 'react'
+import { defaultAppSettings } from './AppSettings.jsx'
 export const AppContext = createContext(defaultAppSettings())
 export const useApp = () => React.useContext(AppContext)
 export const useAppSettings = () => React.useContext(AppContext)
