21400bbd0e768272afbd03f31fa7b9a6b3f68292
Oskars Sylwan <oskars.sylwan@ossy.se>
2026-03-10 18:46:44 +0100

    fix(cli): import

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable global-require, no-unused-vars */
-import ResourceTemplates from './resource-templates/cli.js'
+import * as ResourceTemplates from './resource-templates/cli.js'
 
 const [_, __, handlerName, ...restArgs] = process.argv
 
